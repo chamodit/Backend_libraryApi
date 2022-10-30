85e79a47fa5ded7a6116a5fc4b125d6a537c2026
IT18231724 <58078417+chamodit@users.noreply.github.com>
2022-10-30 21:20:43 +0530

    Completed Backend

--- utill/database.js
+++ utill/database.js
@@ -4,6 +4,7 @@ const config = require("../config/database.json");
 //const username = config.username;
 const password = config.password;
 const url = config.url;
+const host = config.host;
 
 const connect = () => mongoose.connect(url.replace("<password>", password));
 
